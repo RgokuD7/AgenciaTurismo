0854fc7d86dc9b89199dc6a8a5e3dfeb764fec92
RgokuD7 <richarddiaz0107@gmail.com>
2023-04-29 00:37:14 -0400

    Clima listo

--- clima.js
+++ clima.js
@@ -17,7 +17,17 @@ $(document).ready(function () {
         const dia = fecha.getDate();
         const fechaTexto = `${mes}/${dia}`;
         const diaActual = diasSemana[fecha.getDay()];
-        $(".clima-hoy").append("<h2>" + diaActual + " " + fechaTexto + "</h2><img src='" + item.weather.icon + "'><h3>" + item.temp + "°C</h3>");
+        $(".clima-hoy").append(
+          "<h2> Hoy " +
+            diaActual +
+            " " +
+            fechaTexto +
+            "</h2><img src='weatherbit_api/icons/" +
+            item.weather.icon +
+            ".png'><h3>" + item.weather.description + "</h3><h3>" +
+            Math.round(item.temp) +
+            "°C</h3>"
+        );
       });
     }
   );
@@ -25,7 +35,15 @@ $(document).ready(function () {
     "https://api.weatherbit.io/v2.0/forecast/daily?city_id=3874787&lang=es&days=10&key=71c1eedf6fcb41b68219f19c92fcab7e",
     function (data) {
       $.each(data.data, function (i, item) {
-        if (i >= 1) {
+        if ( i == 0){
+          $(".clima-hoy").append(
+            "<p> Min: " +
+              Math.round(item.min_temp) +
+              "°C Max: " +
+              Math.round(item.max_temp) +
+              "°C</p>"    
+          );
+        }else{
           const fecha = new Date(item.datetime);
           const mes = fecha.getMonth() + 1;
           const dia = fecha.getDate();
@@ -36,11 +54,14 @@ $(document).ready(function () {
               diaActual +
               " " +
               fechaTexto +
+              "</h2><img src='weatherbit_api/icons/" +
+            item.weather.icon +
+            ".png'><h3>" + item.weather.description + "</h3>" +
               "</h2><p> Min: " +
               Math.round(item.min_temp) +
-              " Max: " +
+              "°C Max: " +
               Math.round(item.max_temp) +
-              "</p></div>"
+              "°C </p></div>"
           );
         }
       });
